77f752a63a15997e638e907a4907d4596ca67a0f
Nahalu <nahalu@localhost.localdomain>
2020-04-17 06:41:46 +0200

    :sparkles: implement first button storybook

---
+++ src/app/buttons/buttons.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Basic buttons
+ */
+@Component({
+  selector: 'alx-button',
+  templateUrl: 'buttons.component.html',
+  styleUrls: ['buttons.component.scss'],
+})
+
+export class ButtonsComponent {}
