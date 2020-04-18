b16fa217ffd7e61914ada5b5aa7164f93e80d807
Nahalu <romain.mard@gmail.com>
2020-04-19 00:36:20 +0200

    :building_construction: reimplement project

---
+++ projects/enneagone-angular-ds/src/lib/enneagone-angular-ds.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ui-enneagone-angular-ds',
+  template: `
+    <p>
+      enneagone-angular-ds works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class EnneagoneAngularDsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
