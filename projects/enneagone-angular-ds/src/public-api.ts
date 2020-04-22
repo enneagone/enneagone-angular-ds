a4977433bff19a6f0f614ceaa511ee18886df724
croquelois <croquelois.adrien@gmail.com>
2020-04-22 15:42:33 +0200

    :construction: work on button

--- projects/enneagone-angular-ds/src/public-api.ts
+++ projects/enneagone-angular-ds/src/public-api.ts
@@ -2,5 +2,7 @@
  * Public API Surface of enneagone-angular-ds
  */
 
-export * from './lib/e9-btn/e9-btn.component';
+export * from './lib/button/button.component';
+
+export * from './lib/button/button.module';
 
