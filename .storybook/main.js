a1ce210121479e1b4d838a638ef19c8d2412fd53
croquelois <croquelois.adrien@gmail.com>
2020-05-07 02:32:39 +0200

    :bug: fix addon

---
+++ .storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  stories: ['../src/stories/*.stories.ts'],
+  addons: [
+    '@storybook/addon-actions/register',
+    '@storybook/addon-links/register',
+    '@storybook/addon-notes/register',
+    '@storybook/addon-knobs/register'
+  ]
+};
