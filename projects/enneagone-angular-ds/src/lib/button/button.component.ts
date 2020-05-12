500a130f13ce0638a49442e43338b8490e5639ef
croquelois <croquelois.adrien@gmail.com>
2020-05-12 08:26:24 +0200

    :sparkles: Custom button

--- projects/enneagone-angular-ds/src/lib/button/button.component.ts
+++ projects/enneagone-angular-ds/src/lib/button/button.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'e9-button',
@@ -6,18 +6,56 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
   styleUrls: ['./button.component.css']
 })
 
-export class ButtonComponent {
+export class ButtonComponent implements OnInit {
   @Input() value: string;
-  @Input() color: string;
-  @Input() textColor: string;
-  @Input() disabled: boolean;
+  @Input() color = '#7C72A0';
+  @Input() textColor = '#FFFFFF';
+  @Input() disabled = false;
+  @Input() outlined = false;
   @Input() type: 'button' | 'submit' | 'a';
+  @Input() borderColor: string;
+  @Input() set size(value: ('xs' | 'sm' | 'md' | 'lg' | 'xl')) {
+    switch (value) {
+      case 'xs':
+        this.fontSize = '0.8em';
+        this.width = '4.5em';
+        this.height = '2em';
+        break;
+      case 'sm':
+        this.fontSize = '0.9em';
+        this.width = '4.9em';
+        this.height = '2.3em';
+        break;
+      case 'md':
+        this.fontSize = '1em';
+        this.width = '6em';
+        this.height = '2.2em';
+        break;
+      case 'lg':
+        this.fontSize = '1.1em';
+        this.width = '7em';
+        this.height = '2.6em';
+        break;
+      case 'xl':
+        this.fontSize = '1.3em';
+        this.width = '9em';
+        this.height = '3em';
+        break;
+    }
+  }
   @Output() btnClick: EventEmitter<any> = new EventEmitter();
+  private width: string;
+  private height: string;
+  private fontSize: string;
+  private border = '0.05em solid';
+  private test = 'black';
   constructor() {
-    this.disabled = false;
-    // TODO: Set up les couleurs par defaut sur la lib
-    this.color = '#7C72A0';
-    this.textColor = '#FFFFFF';
     this.type = 'button';
+    this.size = 'md';
+  }
+  ngOnInit(): void {
+    if (this.borderColor == null) {
+      this.borderColor = this.color;
+    }
   }
 }
--- src/app/components/overview-button/overview-button.module.ts
+++ src/app/components/overview-button/overview-button.module.ts
@@ -1,12 +1,16 @@
 import {NgModule} from '@angular/core';
 import {OverviewButtonComponent} from './overview-button.component';
+import {ButtonModule} from '../../../../projects/enneagone-angular-ds/src/public-api';
 
 @NgModule({
-  declarations: [
-    OverviewButtonComponent
-  ],
-  exports: [
-    OverviewButtonComponent
-  ]
+    declarations: [
+        OverviewButtonComponent
+    ],
+    imports: [
+        ButtonModule
+    ],
+    exports: [
+        OverviewButtonComponent
+    ]
 })
 export class OverviewButtonModule { }
