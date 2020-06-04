b105340b2fafb53d645695ce2cb6cb4d76730892
croquelois <croquelois.adrien@gmail.com>
2020-06-04 23:27:04 +0200

    :bug: Fix input control value

--- projects/enneagone-angular-ds/src/lib/input/input.component.ts
+++ projects/enneagone-angular-ds/src/lib/input/input.component.ts
@@ -1,12 +1,13 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {ControlValueAccessor, NgModel} from '@angular/forms';
+import {AfterViewInit, Component, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
+import {ControlValueAccessor, DefaultValueAccessor, NgModel} from '@angular/forms';
+import {ReplaySubject} from "rxjs";
 
 @Component({
   selector: 'e9-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.css']
 })
-export class InputComponent implements OnInit, ControlValueAccessor {
+export class InputComponent implements ControlValueAccessor, AfterViewInit {
   @Output() ngValueChange: EventEmitter<any> = new EventEmitter();
   @Input() value: NgModel;
   @Input() label = '';
@@ -18,14 +19,33 @@ export class InputComponent implements OnInit, ControlValueAccessor {
   @Input() color = '#E0E0E0';
   @Input() labelColor = 'black';
   @Input() type: 'password' | 'text' | 'number';
+  @Input() formControlName: string;
+  @ViewChild(DefaultValueAccessor) valueAccessor: DefaultValueAccessor;
   constructor() {
     this.type = 'text';
   }
 
-  ngOnInit(): void {
+  delegatedMethodCalls = new ReplaySubject<(_: ControlValueAccessor) => void>();
+  emitValueChange({$event}: { $event: any }) {
+    this.ngValueChange.emit($event);
   }
 
-  emitValueChange($event: any) {
-    this.ngValueChange.emit($event);
+  ngAfterViewInit(): void {
+    this.delegatedMethodCalls.subscribe(fn => fn(this.valueAccessor));
+  }
+
+  registerOnChange(fn: (_: any) => void): void {
+    this.delegatedMethodCalls.next(valueAccessor => valueAccessor.registerOnChange(fn));
+  }
+  registerOnTouched(fn: () => void): void {
+    this.delegatedMethodCalls.next(valueAccessor => valueAccessor.registerOnTouched(fn));
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.delegatedMethodCalls.next(valueAccessor => valueAccessor.setDisabledState(isDisabled));
+  }
+
+  writeValue(obj: any): void {
+    this.delegatedMethodCalls.next(valueAccessor => valueAccessor.writeValue(obj));
   }
 }
